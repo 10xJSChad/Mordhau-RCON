24f464d51247589e6cf1cb266cf5da21a4b6651a
10xJSChad <48174610+10xJSChad@users.noreply.github.com>
2021-08-12 23:59:26 +0200

    Update rcon shell.py

--- rcon shell.py
+++ rcon shell.py
@@ -3,7 +3,7 @@ from library.client import client as rconClient
 
 client = rconClient(shell_mode=True)
 client.connect(use_json=False)
-client.start_listen()
+client.start_listen(keepAlive=True)
 
 while True:
     client.run(input())
